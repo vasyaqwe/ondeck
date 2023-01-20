4e65b9d6d729b644ba59a6b5aca17b447d269816
vasyaqwe <elekton.fafik@gmail.com>
2023-01-20 23:37:30 +0200

    working on mobile menu

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const mobileNav = document.querySelector('.nav--mobile')
 const mobileNavToggle = document.querySelector('.toggle--nav')
+const mobileNavBtns = document.querySelectorAll('.nav--mobile__btn')
+const navPanelsContainer = document.querySelector('.nav-panels')
 
 mobileNavToggle.addEventListener('click', (e) => {
   mobileNavToggle.setAttribute('aria-expanded', !mobileNav.hasAttribute('data-visible'))
   mobileNav.toggleAttribute('data-visible')
 })
+mobileNavBtns.forEach(btn => btn.addEventListener('click', (e) => {
+  const targetPanel = document.querySelector(`#${e.target.getAttribute('aria-controls')}`)
+  navPanelsContainer.setAttribute('data-visible', true)
+  targetPanel.setAttribute('data-visible', true)
+}))
